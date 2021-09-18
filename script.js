70d797341efaa153fcd3d48dd8310be3c46fc6ad
dennistuk95 <88230815+dennistuk95@users.noreply.github.com>
2021-09-18 12:18:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hallo Winc Academy studenten")
+
+/* Grote comment
+
+// Dit is een grote som
+function multiplyNumbers(x, y) {
+    let product = x * y;
+    console.log(product);
+}
+
+multiplyNumbers(1230941, 1823794);
+
+
+// Som 2
+function divideNumbers(x, y) {
+    let solution = x / y;
+    console.log(solution);
+}
+
+divideNumbers(637263, 54); */
